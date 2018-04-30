aa2596231c32e0a943cac8b06c23562b7a239481
miyake <miyake.y@me.com>
2018-04-30 17:23:01 +0900

    fixed sh

--- create-lamda-upload-zip.sh
+++ create-lamda-upload-zip.sh
@@ -1,7 +1,12 @@
-/bin/bash
+#!/bin/bash
 
 script_dir=$(cd $(dirname $0) && pwd)
+zip_file=${script_dir}/upload.zip
 cd $script_dir/amazon-lambda
 
-zip -r ../upload.zip *
+npm install
+if [ -e ${zip_file} ]; then
+  rm -fv ${zip_file}
+fi
+zip -r ${zip_file} *
 
